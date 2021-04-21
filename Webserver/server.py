a2be419a56fe9485a127b8e76725d64128e278cc
sszxc <im.zhangxc@foxmail.com>
2021-04-21 18:03:37 +0800

    添加Flask框架；百度地图API测试

---
+++ Webserver/server.py
@@ -0,0 +1,20 @@
+# 用Python编写一个桌面软件系统的步骤是什么？ - dc lin的回答 - 知乎
+# https://www.zhihu.com/question/338453063/answer/773655748
+
+import flask as fk
+
+app = fk.Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if fk.request.method == "POST":
+        feet = float(fk.request.form['entry_feet'])
+        meters = feet * 0.3048
+        return fk.render_template("index.html", meters=meters, feet=feet)
+    else:
+        return fk.render_template("index.html", meters=0.3048, feet=1)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", debug=True)
--- src/test.py
+++ src/test.py
@@ -1,3 +1,3 @@
-print(0 or "hello")
-path = '1234567'
-print(path[2:])
+# print(0 or "hello")
+# path = '1234567'
+# print(path[2:])
